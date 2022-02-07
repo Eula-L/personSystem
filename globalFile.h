86245d039403f747800e146f0b917cbee64e794c
刘昊 <88362269+liuhao-2002@users.noreply.github.com>
2022-02-07 13:06:38 +0800

    简化用户流程

--- globalFile.h
+++ globalFile.h
@@ -1,12 +1,8 @@
 #pragma once
 
-//管理员文件
-#define MANAGER_FILE "Manager.txt"
-//流动人员文件
-#define MOBILE_FILE "Mobile.txt"
-//普通人员文件
-#define ORDINARY_FILE "Ordinary.txt"
+//账号信息文件
+#define PEOPLE "People.txt"
 //上报信息文件
 #define INFOMATION "Infomation.txt"
 //公告文件
-#define NOTICE_FILE "Notice.txt"
+#define NOTICE "Notice.txt"
--- 疫情期间流动人员管理系统.cpp
+++ 疫情期间流动人员管理系统.cpp
@@ -1,117 +1,78 @@
-﻿#include<iostream>
-#include<fstream>
-#include<string>
+﻿#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-#include"Identity.h"
-#include"globalFile.h"
-#include"Ordinary.h"
-#include"Mobile.h"
-#include"Manager.h"
+#include "Identity.h"
+#include "globalFile.h"
+#include "Ordinary.h"
+#include "Mobile.h"
+#include "Manager.h"
 
 //by:北辰
-
-//登录LoginIn
-void LoginIn(string fileName,int type)
+void LoginIn(string fileName, string name, string pwd, bool val);
+//准备登录
+void log(bool boo)
 {
-	//父类指针，用于指向子类对象
-	Identity* person = NULL;
-	//读文件
-	ifstream ifs;
-	ifs.open(fileName, ios::in);
-	//判断文件是否存在
-	if (!ifs.is_open())
-	{
-		cout << "文件不存在" << endl;
-		ifs.close();
-		return;
-	}
-	//准备接受用户的信息
 	string name;
 	string pwd;
 	cout << "请输入姓名" << endl;
 	cin >> name;
 	cout << "请输入密码" << endl;
 	cin >> pwd;
-
-	//验证登录
-	if (type == 1)
-	{
-		//普通人验证登录
-		string fName;
-		string fPwd;
-		bool val = false;//用于记录登录验证
-		while (ifs >> fName && ifs >> fPwd)
-		{
-			if (name == fName && pwd == fPwd)
-			{
-				cout << "普通人验证登录成功！" << endl;
-				val = true;
-				system("pause");
-				system("cls");
-				person = new Ordinary(name, pwd);
-				return;
-			}
-		}
-		if (!val)
-		{
-			cout << "用户名或密码有误，请重新输入" << endl;
-			LoginIn(ORDINARY_FILE, 1);
-			return;
-		}
-	}
-	else if (type == 2)
-	{
-		//流动人员验证登录
-		string fName;
-		string fPwd;
-		bool val = false;//用于记录登录验证
-		while (ifs >> fName && ifs >> fPwd)
-		{
-			if (name == fName && pwd == fPwd)
-			{
-				cout << "流动人员验证登录成功！" << endl;
-				system("pause");
-				system("cls");
-				person = new Mobile(name, pwd);
-				return;
-			}
-		}
-		if (!val)
-		{
-			cout << "用户名或密码有误，请重新输入" << endl;
-			LoginIn(MOBILE_FILE, 2);
-			return;
-		}
-	}
-	else if (type == 3)
-	{
-		//管理员验证登录
-		string fName;
-		string fPwd;
-		bool val = false;//用于记录登录验证
-		while (ifs >> fName && ifs >> fPwd)
-		{
-			if (name == fName && pwd == fPwd)
-			{
-				cout << "管理员验证登录成功!" << endl;
-				//登录成功后，按任意键进入管理员界面
-				system("pause");
-				system("cls");
-				//创建管理员对象
-				person = new Manager(name, pwd);
-				return;
-			}
-		}
-		if (!val)
-		{
-			cout << "用户名或密码有误，请重新输入" << endl;
-			LoginIn(MANAGER_FILE, 3);
-			return;
-		}
-		
-	}
-
+	system("cls");
+	LoginIn(PEOPLE, name, pwd, boo);
 }
+
+//登录LoginIn
+void LoginIn(string fileName, string name, string pwd, bool val)
+{
+	//父类指针，用于指向子类对象
+	Identity* person = NULL;
+	//读文件
+	ifstream ifs;
+	ifs.open(fileName, ios::in);
+
+	string fName;
+	string fPwd;
+	int shenfen = 0;
+	while (ifs >> fName && ifs >> fPwd && ifs >> shenfen)
+	{
+		if (name == fName && pwd == fPwd)
+		{
+			val = true;
+			if (shenfen == 1)
+			{
+				cout << "普通人";
+				person = new Ordinary(name, pwd);
+				//进入普通人子页面
+			}
+			else if (shenfen == 2)
+			{
+				cout << "流动人员";
+				person = new Ordinary(name, pwd);
+				//
+			}
+			else if (shenfen == 3)
+			{
+				cout << "管理员";
+				person = new Ordinary(name, pwd);
+				//
+			}
+			cout << "验证登录成功！" << endl;
+
+			system("pause");
+			system("cls");
+
+			return;
+		}
+	}
+	if (!val)
+	{
+		cout << "用户名或密码错误\n请重新输入密码" << endl;
+		log( val);
+	}
+}
+
 int main()
 {
 	//创建一个变量记录选择
@@ -130,42 +91,12 @@ int main()
 		cout << "\t\t -------------------------------\n";
 		cout << "请输入您的选择：\n";
 		cin >> choose;
+		bool val = false;
 		switch (choose)
 		{
 		case 1://登录
-			system("cls");
-			while (true)
-			{
-				int type;//用于记录选择的类
-				cout << "\t\t -------------------------------\n";
-				cout << "\t\t|                               |\n";
-				cout << "\t\t|          1.普 通 人            |\n";
-				cout << "\t\t|                               |\n";
-				cout << "\t\t|          2.流动人员            |\n";
-				cout << "\t\t|                               |\n";
-				cout << "\t\t|          3.管理员              |\n";
-				cout << "\t\t|                               |\n";
-				cout << "\t\t -------------------------------\n";
-				cout << "请输入您的账户类型：\n";
-				cin >> type;
-				switch (type)
-				{
-				case 1://普通人
-					LoginIn(ORDINARY_FILE, 1);
-					break;
-				case 2://流动人员
-					LoginIn(MOBILE_FILE, 2);
-					break;
-				case 3://管理员
-					LoginIn(MANAGER_FILE, 3);
-					break;
-				default:
-					cout << "输入有误，请重新选择！" << endl;
-					system("pause");
-					system("cls");//刷新界面，相当于删除错误输出和提示
-					break;
-				}
-			}
+			//准备接受用户的信息
+			log(val);
 			break;
 		case 2://注册
 			break;
